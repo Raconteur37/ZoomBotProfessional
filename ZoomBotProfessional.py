9652762cfbf43639475c77e685501fa1a08cefb3
Cam <69336066+Raconteur37@users.noreply.github.com>
2021-11-30 07:40:18 -0600

    Added bubble sort method for classes

--- ZoomBotProfessional.py
+++ ZoomBotProfessional.py
@@ -37,6 +37,39 @@ class Class:
     def getLink(self):
         return self.link
 
+def sortClasses(a:list):
+    del a[0]
+    for x in range(len(a)):
+        for z in range(len(a) - 1):
+            temp = a[z]
+            #tempClass = getInstanceFromDb(temp, 1) #Class name
+            tempTime = getInstanceFromDb(temp,2) # Class time
+            tempTime = tempTime.split(":")
+            tempTimeHour = tempTime[0]
+            tempTimeMinute = tempTime[1]
+            finalTime = float(tempTimeMinute) * .01
+            finalTime = finalTime + float(tempTimeHour)
+            tempTimeofDay = getInstanceFromDb(temp,3) # Class time of day
+            if str.lower(tempTimeofDay) == "pm":
+                if (int(tempTimeHour) != 12):
+                    finalTime = float(finalTime) + 12.0
+            temp2 = a[z + 1]
+            #tempClass2 = getInstanceFromDb(temp2, 1) #Class name
+            tempTime2 = getInstanceFromDb(temp2,2) # Class time
+            tempTime2 = tempTime2.split(":")
+            tempTimeHour2 = tempTime2[0]
+            tempTimeMinute2 = tempTime2[1]
+            finalTime2 = float(tempTimeMinute2) * .01
+            finalTime2 = finalTime2 + float(tempTimeHour2)
+            tempTimeofDay2 = getInstanceFromDb(temp2,3) # Class time of day
+            if str.lower(tempTimeofDay2) == "pm":
+                if(int(tempTimeHour2) != 12):
+                    finalTime2 = float(finalTime2) + 12.0        
+            if (float(finalTime) > float(finalTime2)):
+                a[z], a[z+1] = a[z+1], a[z]
+    return a
+
+
 def getInstanceFromDb(a:str,index):
     a = str(a)
     a = a.replace("[","")
@@ -60,19 +93,22 @@ temp = personName.split(" ")
 print(f"{colors.BOLD}Welcome {colors.YELLOW}{temp[0]}.{colors.END}")
 run = True
 while (run):
-    print(f"{colors.CYAN}Choose some options: {colors.END}\n 1. List all classes to access \n 2. Add a class \n 3. Edit a class \n 4. Remove a class \n 5. {colors.WARNING}{colors.BOLD}Exit{colors.END}")
+    print(f"{colors.CYAN}{colors.BOLD}Choose some options: {colors.END}\n 1. List all classes to access \n 2. Add a class \n 3. Edit a class \n 4. Remove a class \n 5. {colors.WARNING}{colors.BOLD}Exit{colors.END}")
     option = int(input("Choose an option: "))
 
     if (option == 1):
         c.execute('SELECT * FROM classes WHERE name IN (SELECT name FROM classes WHERE name = ?)', (personName,))
         check = c.fetchall()
+        check = sortClasses(check)
         if len(check) > 1:
             print(f"Here is a list of your classes: ")
             print(f"")
-            z = 1
+            disNum = 1
+            z = 0
             while (z < len(check)):
-                print(f"-={colors.BOLD}{colors.UNDERLINE} {z}. {getInstanceFromDb(check[z],1)} at {getInstanceFromDb(check[z],2)} {getInstanceFromDb(check[z],3)} {colors.END}=-")
+                print(f"-={colors.BOLD}{colors.UNDERLINE} {disNum}. {getInstanceFromDb(check[z],1)} at {getInstanceFromDb(check[z],2)} {getInstanceFromDb(check[z],3)} {colors.END}=-")
                 z = z + 1
+                disNum = disNum + 1
             print(f"")
         else:
             print("You have no classes!")
@@ -102,47 +138,60 @@ while (run):
         if len(check) > 1:
             print("Here are your classes: ")
             z = 1
+            classNames = []
+            go = False
             while (z < len(check)):
                 print(f"{colors.UNDERLINE}{z}. {getInstanceFromDb(check[z],1)} at {getInstanceFromDb(check[z],2)} {getInstanceFromDb(check[z],3)} with a link of {getInstanceFromDb(check[z],4)}{colors.END}")
+                classNames.append(str.lower((getInstanceFromDb(check[z],1))))
                 z = z + 1
             edit = input("What class would you like to edit? (The exact name that's displayed): ")
             edit = edit.replace(" ","")
-            editType = input("What do you want to edit? EX (name, time, time of day, or link): ")
-            if (str.lower(editType) == "name"):
-                editName = input(f"What do you want the new name for {edit} to be? ")
-                with conn:
-                    c.execute(
-                    'UPDATE classes SET class = ? WHERE class= ?', 
-                    (editName, edit,))
-                print(f"Updated {edit} to now be called {editName}")
-            if (str.lower(editType) == "time"):
-                editTime = input(f"What do you want the new time for {edit} to be? EX (10:30): ")
-                with conn:
-                    c.execute(
-                    'UPDATE classes SET time = ? WHERE class= ?', 
-                    (editTime, edit,))
-                print(f"Updated {edit}'s time to {editTime}")
-            if (str.lower(editType) == "time of day"):
-                editTimeOfDay = input(f"What do you want the new time of day for {edit} to be? EX (AM or PM): ")
-                with conn:
-                    c.execute(
-                    'UPDATE classes SET timeofday = ? WHERE class= ?', 
-                    (editTimeOfDay, edit,))
-                print(f"Updated {edit}'s time of day to {editTimeOfDay}")
-            if (str.lower(editType) == "link"):
-                editLink = input(f"What do you want the new link for {edit} to be? EX (zoom.com): ")
-                with conn:
-                    c.execute(
-                    'UPDATE classes SET link = ? WHERE class= ?', 
-                    (editLink, edit,))
-                print(f"{colors.BOLD}{colors.SUCCESS}Updated {edit}'s link to {editLink}{colors.END}")
+            testEdit = str.lower(edit)
+            v = 0
+            while v < len(classNames):
+                if classNames[v] == testEdit:
+                    go = True
+                    v = len(classNames)
+                v = v + 1
+            if(go):
+                editType = input("What do you want to edit? EX (name, time, time of day, or link): ")
+                if (str.lower(editType) == "name"):
+                    editName = input(f"What do you want the new name for {edit} to be? ")
+                    with conn:
+                        c.execute(
+                        'UPDATE classes SET class = ? WHERE class= ?', 
+                        (editName, edit,))
+                    print(f"Updated {edit} to now be called {editName}")
+                if (str.lower(editType) == "time"):
+                    editTime = input(f"What do you want the new time for {edit} to be? EX (10:30): ")
+                    with conn:
+                        c.execute(
+                        'UPDATE classes SET time = ? WHERE class= ?', 
+                        (editTime, edit,))
+                    print(f"Updated {edit}'s time to {editTime}")
+                if (str.lower(editType) == "time of day"):
+                    editTimeOfDay = input(f"What do you want the new time of day for {edit} to be? EX (AM or PM): ")
+                    with conn:
+                        c.execute(
+                        'UPDATE classes SET timeofday = ? WHERE class= ?', 
+                        (editTimeOfDay, edit,))
+                    print(f"Updated {edit}'s time of day to {editTimeOfDay}")
+                if (str.lower(editType) == "link"):
+                    editLink = input(f"What do you want the new link for {edit} to be? EX (zoom.com): ")
+                    with conn:
+                        c.execute(
+                        'UPDATE classes SET link = ? WHERE class= ?', 
+                        (editLink, edit,))
+                    print(f"{colors.BOLD}{colors.SUCCESS}Updated {edit}'s link to {editLink}{colors.END}")
+                else:
+                    print(f"{colors.WARNING}{colors.BOLD}That's not a valid argument!{colors.END}")
             else:
-                print(f"{colors.WARNING}{colors.BOLD}That's not a valid argument!{colors.END}")
+                print(f"{colors.WARNING}{colors.BOLD}That's not a class!{colors.END}")        
         else:
             
             print(f"{colors.WARNING}{colors.BOLD}You dont have any classes! {colors.END}")
 
-    if (option == 5):
+    if (option == 4):
         print()
         
 
